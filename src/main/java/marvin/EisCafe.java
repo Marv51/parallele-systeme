735b466d3041bc6e61443392bc4aaa12e577c7d2
Marvin Rühe <marvin@server-fish.de>
2017-11-10 00:09:29 +0100

    Move to Exevuters and Locks for Task 2

--- src/main/java/marvin/EisCafe.java
+++ src/main/java/marvin/EisCafe.java
@@ -2,10 +2,22 @@ package marvin;
 
 import java.util.Date;
 import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class EisCafe{
 
+        private static Executor pool = Executors.newFixedThreadPool(5);
+
         Random r;
+
+        private final ReentrantLock serverLock = new ReentrantLock();
+        private final Condition serverLockCondition = serverLock.newCondition();
+
+        private final ReentrantLock seatLock = new ReentrantLock();
+        private final ReentrantLock customerIdLock = new ReentrantLock();
 
         private int freeSeats = 20;
         private int peopleInQueue = 0;
@@ -22,13 +34,17 @@ public class EisCafe{
         }
 
         public void GetServer(){
-            synchronized(this){
+            serverLock.lock();
+            try{
                 while (availableServers == 0){
                     try{
-                        wait();
+                        serverLockCondition.await();
                     } catch (InterruptedException ex){};
                 }
                 availableServers--;                    
+            }
+            finally {
+                serverLock.unlock();
             }
         }
 
@@ -39,34 +55,45 @@ public class EisCafe{
         }
 
         public void ReturnServer(){
-            synchronized(this){
+            serverLock.lock();
+            try{
                 availableServers++;
-                notify();
+                serverLockCondition.signal();
+            }
+            finally{
+                serverLock.unlock();
             }
         }
 
         public void CustomerLeave(){
-            synchronized(this){
+            seatLock.lock();
+            try{
                 freeSeats++;
                 if (peopleInQueue > 0){
                     createNewCustomers(1);
                     peopleInQueue--;
                 }
             }
+            finally{
+                seatLock.unlock();
+            }
         }
 
         private void createNewCustomers(int cus){
             for (int i = 0; i < cus; i++){
-                synchronized (this){
-                    Thread c = new Thread( new Kunde(this, currentCustomerId++) );
-                    c.start();
+                customerIdLock.lock();
+                try{
+                    pool.execute(new Kunde(this, currentCustomerId++));
+                } finally {
+                    customerIdLock.unlock();
                 }
             }
         }
 
         public void NewCustomers(){
             int newCustomers = r.nextInt(5) + 1;
-            synchronized(this){
+            seatLock.lock();
+            try{
                 if (freeSeats < newCustomers){
                     peopleInQueue += (newCustomers - freeSeats);
                     createNewCustomers(freeSeats);
@@ -75,6 +102,8 @@ public class EisCafe{
                     createNewCustomers(newCustomers);
                     freeSeats -= newCustomers;
                 }
+            } finally {
+                seatLock.unlock();
             }
             System.out.println(TimeSinceStart() + "Neuer Kunde! Aktuell " + freeSeats + " freie Plätze verfügbar und " + peopleInQueue + " Kunden in der Warteschlange.");
         }
@@ -84,11 +113,9 @@ public class EisCafe{
              System.out.println("Willkommen im Eiscafe");
              EisCafe s = new EisCafe();
              ArrivingCustomers ac = new ArrivingCustomers(s);
-             Thread act = new Thread(ac);
-             act.start();
+             pool.execute(ac);
              System.out.println("Bitte Enter zum Beenden drücken.");
              System.console().readLine();
              ac.Shutdown();
-             act.interrupt();
         }
 }
